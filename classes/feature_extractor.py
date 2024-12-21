4a2f8c7380049a242b354aea217937a1b707e8c7
yazan AbuAloun <61243898+yazan6546@users.noreply.github.com>
2024-12-21 15:42:12 +0000

    ORB and SIFT now support pickle

--- classes/feature_extractor.py
+++ classes/feature_extractor.py
@@ -37,3 +37,34 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
             return descriptors
 
         return X.apply(extract)
+
+    
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        # Remove the cv2.ORB or cv2.SIFT object from the state
+        state['detector'] = None
+        return state
+    
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        # Recreate the cv2.ORB or cv2.SIFT object
+        if self.method == 'ORB':
+            self.detector = cv2.ORB_create(
+                                nfeatures=750,
+                                scaleFactor=1.2,
+                                nlevels=8,
+                                edgeThreshold=15,
+                                firstLevel=0,
+                                WTA_K=2,
+                                scoreType=cv2.ORB_HARRIS_SCORE,
+                                patchSize=15,
+                                fastThreshold=10
+                            )
+        elif self.method == 'SIFT':
+            self.detector = cv2.SIFT_create(
+                                nfeatures=0,
+                                nOctaveLayers=3,
+                                contrastThreshold=0.01,
+                                edgeThreshold=15,
+                                sigma=1.2
+                            )  
